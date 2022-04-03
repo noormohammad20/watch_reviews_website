9829f5473fa13e23203d84d91e9ee959fbbdad24
noormohammad20 <mdsaim204@gmail.com>
2022-04-03 22:06:39 +0600

    not found page complete

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NotFound.css'
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>ops!! Page Not Available</h2>
+        </div>
+    )
+}
+
+export default NotFound
