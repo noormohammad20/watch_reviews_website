2752f57de2132936901bd4dfdcc7ac55ea7299da
noormohammad20 <mdsaim204@gmail.com>
2022-04-04 01:12:18 +0600

    show data on reviews component

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import useReview from '../../hooks/useReview'
 import './Reviews.css'
 const Reviews = () => {
+    const [reviews] = useReview()
     return (
         <div>
-            <h2>This Is Reviews Page</h2>
+            {
+                reviews.map(review =>
+                    <div className='review-container'>
+                        <div>
+                            <img className="review-img" src={review.picture} alt="" />
+                        </div>
+                        <div className="review-info">
+                            <h4>Name: {review.name}</h4>
+                            <p>Review: {review.review}</p>
+                            <p><small>Ratings: {review.ratings} Stars</small></p>
+                        </div>
+
+
+                    </div>
+                )
+            }
         </div>
     )
 }
